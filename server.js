4ee986c1681110286abf9a4da729915b36810e72
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-18 19:46:50 -0300

    master

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const exp = require("constants");
 const express = require("express");
 const fs = require ("fs");
 const mysql = require ("mysql");
@@ -10,5 +11,18 @@ const db = mysql.createConnection({
 //connect
 db.connect (err => {
     if (err) throw err;
-    console.timeLog("MySql connected")
+    console.timeLog("MySql connected");
+})
+
+//init app
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(3000, () => {
+    console.log("server has started on port 3000");
 })
